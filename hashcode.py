d5637ee36ac3fb5ba869e56066aaaf87d235d505
Raghav Dhingra <raghav.dhingra15@gmail.com>
2019-04-15 21:14:45 +0530

    Add files via upload

--- hashcode.py
+++ hashcode.py
@@ -2,3 +2,15 @@ countH = 0
 countV = 0
 arr2 = []
 arr3 = []
+inp = int(input())
+for i in range(inp):
+    arr = input().split(" ")
+    if arr[0]=='H':
+        countH+=1
+        arr2.append(i)
+    elif arr[0]=='V':
+        countV+=1
+countV*=0.5
+print(int(countH+countV))
+for cc in range(len(arr2)):
+    print(arr[cc])
